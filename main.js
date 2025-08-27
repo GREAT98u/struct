de649361bbfda0b6474bb8ff8e378fdb5cf21a2e
GREAT98u <anshulgreat987@gmail.com>
2025-08-27 15:35:39 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1100,
+    height: 720,
+    title: "Collab Pad",
+    webPreferences: {
+      contextIsolation: true,
+      sandbox: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
